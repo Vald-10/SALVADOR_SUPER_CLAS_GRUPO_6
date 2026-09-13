0b1857c3c5b03182eb76312a10fd41c0c73d7b81
VIVOBOOK <VIVOBOOK@DESKTOP-IFR4D01>
2026-09-13 11:10:05 -0400

    Agregar archivos de proyecto.

---
+++ SALVADOR_SUPER_CLAS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SALVADOR_SUPER_CLAS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
